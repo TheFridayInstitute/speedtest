007712a408462a55a1ccc5d5805d7c3e306603a8
Michael Babb <mike7400@gmail.com>
2019-11-22 14:05:58 -0500

    Test run of gauges

--- script.js
+++ script.js
@@ -7,58 +7,135 @@ function getUrlVars() {
   return vars;
 }
 
-
-
 // INITIALIZE SPEEDTEST
 var s = new Speedtest();  // create speedtest object
 
-s.onupdate = function(data) {
-  // callback to update data in UI
-  document.getElementById('ip').textContent = data.clientIp;
-  document.getElementById('dlText').textContent =
-      data.testState == 1 && data.dlStatus == 0 ? '...' : data.dlStatus;
-  document.getElementById('ulText').textContent =
-      data.testState == 3 && data.ulStatus == 0 ? '...' : data.ulStatus;
-  document.getElementById('pingText').textContent = data.pingStatus;
-  document.getElementById('jitText').textContent = data.jitterStatus;
+var meterBk = /Trident.*rv:(\d+\.\d+)/i.test(navigator.userAgent) ? '#EAEAEA' :
+                                                                    '#80808040';
+var dlColor = '#6060AA', ulColor = '#616161';
+var progColor = meterBk;
 
-  let urlVars = getUrlVars();
-
-  if (data.testState === 4) {
-    $.post('backend/record.php', {
-      id: urlVars.id,
-      dlStatus: data.dlStatus,
-      ulStatus: data.ulStatus,
-      pingStatus: data.pingStatus,
-      jitterStatus: data.jitterStatus
-    });
-  }
-};
-
-s.onend = function(aborted) {
-  // callback for test ended/aborted
-  document.getElementById('startStopBtn').className =
-      '';  // show start button again
-  if (aborted) {
-    // if the test was aborted, clear the UI and prepare for new test
-    initUI();
-  }
-};
-
-function startStop() {
-  // start/stop button pressed
-  if (s.getState() == 3) {
-    // speedtest is running, abort
-    s.abort();
+// CODE FOR GAUGES
+function drawMeter(c, amount, bk, fg, progress, prog) {
+  var ctx = c.getContext('2d');
+  var dp = window.devicePixelRatio || 1;
+  var cw = c.clientWidth * dp, ch = c.clientHeight * dp;
+  var sizScale = ch * 0.0055;
+  if (c.width == cw && c.height == ch) {
+    ctx.clearRect(0, 0, cw, ch);
   } else {
-    // test is not running, begin
-    s.start();
-    document.getElementById('startStopBtn').className = 'running';
+    c.width = cw;
+    c.height = ch;
+  }
+  ctx.beginPath();
+  ctx.strokeStyle = bk;
+  ctx.lineWidth = 12 * sizScale;
+  ctx.arc(
+      c.width / 2, c.height - 58 * sizScale, c.height / 1.8 - ctx.lineWidth,
+      -Math.PI * 1.1, Math.PI * 0.1);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.strokeStyle = fg;
+  ctx.lineWidth = 12 * sizScale;
+  ctx.arc(
+      c.width / 2, c.height - 58 * sizScale, c.height / 1.8 - ctx.lineWidth,
+      -Math.PI * 1.1, amount * Math.PI * 1.2 - Math.PI * 1.1);
+  ctx.stroke();
+  if (typeof progress !== 'undefined') {
+    ctx.fillStyle = prog;
+    ctx.fillRect(
+        c.width * 0.3, c.height - 16 * sizScale, c.width * 0.4 * progress,
+        4 * sizScale);
   }
 }
 
+function mbpsToAmount(s) {
+  return 1 - 1 / Math.pow(1.3, Math.sqrt(s));
+}
+
+function format(d) {
+  d = Number(d);
+  if (d < 10) return d.toFixed(2);
+  if (d < 100) return d.toFixed(1);
+  return d.toFixed(0);
+}
+
+// UI CODE
+var UI_DATA = null;
+
+function startStop() {
+  if (s.getState() == 3) {
+    // speedtest is running, abort
+    s.abort();
+    data = null;
+    document.getElementById('startStopBtn').className = '';
+    initUI();
+  } else {
+    // test is not running, begin
+    document.getElementById('startStopBtn').className = 'running';
+    s.onupdate = function(data) {
+      UI_DATA = data;
+      let urlVars = getUrlVars();
+
+      if (data.testState === 4) {
+        $.post('backend/record.php', {
+          id: urlVars.id,
+          dlStatus: data.dlStatus,
+          ulStatus: data.ulStatus,
+          pingStatus: data.pingStatus,
+          jitterStatus: data.jitterStatus
+        });
+      }
+    };
+    s.onend = function(aborted) {
+      document.getElementById('startStopBtn').className = '';
+      updateUI(true);
+    };
+    s.start();
+  }
+}
+// this function reads the data sent back by the test and updates the UI
+function updateUI(forced) {
+  if (!forced && s.getState() != 3) return;
+
+  if (UI_DATA == null) return;
+
+  var status = UI_DATA.testState;
+  document.getElementById('ip').textContent = UI_DATA.clientIp;
+  document.getElementById('dlText').textContent =
+      status == 1 && UI_DATA.dlStatus == 0 ? '...' : format(UI_DATA.dlStatus);
+  drawMeter(
+      document.getElementById('dlMeter'),
+      mbpsToAmount(Number(UI_DATA.dlStatus * (status == 1 ? oscillate() : 1))),
+      meterBk, dlColor, Number(UI_DATA.dlProgress), progColor);
+  document.getElementById('ulText').textContent =
+      status == 3 && UI_DATA.ulStatus == 0 ? '...' : format(UI_DATA.ulStatus);
+  drawMeter(
+      document.getElementById('ulMeter'),
+      mbpsToAmount(Number(UI_DATA.ulStatus * (status == 3 ? oscillate() : 1))),
+      meterBk, ulColor, Number(UI_DATA.ulProgress), progColor);
+  document.getElementById('pingText').textContent = format(UI_DATA.pingStatus);
+  document.getElementById('jitText').textContent = format(UI_DATA.jitterStatus);
+}
+function oscillate() {
+  return 1 + 0.02 * Math.sin(Date.now() / 100);
+}
+// update the UI every frame
+window.requestAnimationFrame = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.msRequestAnimationFrame || function(callback, element) {
+      setTimeout(callback, 1000 / 60);
+    };
+function frame() {
+  requestAnimationFrame(frame);
+  updateUI();
+}
+frame();  // start frame loop
 // function to (re)initialize UI
+
 function initUI() {
+  drawMeter(document.getElementById('dlMeter'), 0, meterBk, dlColor, 0);
+  drawMeter(document.getElementById('ulMeter'), 0, meterBk, ulColor, 0);
   document.getElementById('dlText').textContent = '';
   document.getElementById('ulText').textContent = '';
   document.getElementById('pingText').textContent = '';
@@ -66,8 +143,8 @@ function initUI() {
   document.getElementById('ip').textContent = '';
 }
 
-
-
 window.onload = function() {
-  initUI();
+  setTimeout(function() {
+    initUI();
+  }, 100);
 };

7377930d2a603b4f341078311632067e8c226fad
Mike Babb <mike7400@gmail.com>
2026-03-25 20:34:42 -0400

    chore: remove dead composables useGlass and useSSE
    Zero imports found for either. Clean barrel exports.

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -4,6 +4,3 @@ export type { OnMessageCallback } from "./useWindowMessaging";
 export { useSpeedtest } from "./useSpeedtest";
 export { useServerManager } from "./useServerManager";
 export type { ManagedServer } from "./useServerManager";
-export { useResultAggregator, median } from "./useResultAggregator";
-export { useMaplibre } from "./useMaplibre";
-export type { HexData, UseMaplibreOptions } from "./useMaplibre";
